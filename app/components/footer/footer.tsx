acd03dbd5ad38ecea2f8043c6904a5adf6b533c0
Boyeon Jang <uko010199@gmail.com>
2024-11-11 19:51:45 +0900

    feat: update footer

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,7 +1,22 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
-    <footer>
-      <div></div>
+    <footer className="mt-2 mb-6 text-center">
+      <p>
+        &copy; {new Date().getFullYear()}. Japan Festival Calendar. All rights
+        reserved.
+      </p>
+      <div>
+        <Link
+          href={"https://github.com/boyeoon/japan-festival-calendar"}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:underline hover:text-[#cb6872]"
+        >
+          GitHub
+        </Link>
+      </div>
     </footer>
   );
 }
