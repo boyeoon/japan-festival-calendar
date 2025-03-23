14e2a1c752548588ed94ce0005500213dc23a2a3
Boyeon Jang <uko010199@gmail.com>
2025-03-23 18:52:30 +0900

    feat: show detailed info on mouse hover
    Implemented tooltip to display location, time, and description when hovering over an event.

--- app/components/modal/datemodal.tsx
+++ app/components/modal/datemodal.tsx
@@ -1,3 +1,5 @@
+import Info from "@/components/calendar/info";
+
 export default function DateModal({
   isOpen,
   onClose,
@@ -9,10 +11,19 @@ export default function DateModal({
   onClose: () => void;
   date: string;
   events: {
+    id: number;
     title_ja: string;
     title_ko: string;
     title_en: string;
+    date: string;
     link: string;
+    location_ja: string;
+    location_ko: string;
+    location_en: string;
+    time: string;
+    description_ja: string;
+    description_ko: string;
+    description_en: string;
     source: string;
   }[];
   lang: string;
@@ -39,33 +50,73 @@ export default function DateModal({
         {/* 이벤트 목록 */}
         <div className="space-y-3">
           {events.length > 0 ? (
-            events.map((event, index) => (
-              <a
-                key={index}
-                href={event.link}
-                target="_blank"
-                rel="noopener noreferrer"
-                className={`block text-lg text-center px-4 py-2 rounded-md transition
-                  ${
-                    lang === "ko"
-                      ? "font-LINESeedKR"
-                      : lang === "en"
-                      ? "font-LINESeedJP"
-                      : "font-LINESeedEN"
-                  } ${
-                  event.source === "yoyogi"
-                    ? "bg-[#A2D4FF] hover:bg-[#70b9ff]"
-                    : event.source === "bread"
-                    ? "bg-[#FFD6A5] hover:bg-[#ffbe70]"
-                    : "bg-gray-300"
-                }`}
+            events.map((event) => (
+              <div
+                key={`${event.source}-${event.id}`}
+                className="relative group"
               >
-                {lang === "ko"
-                  ? event.title_ko
-                  : lang === "ja"
-                  ? event.title_ja
-                  : event.title_en}
-              </a>
+                <a
+                  href={event.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={`block text-lg text-center px-4 py-2 rounded-md transition
+            ${
+              lang === "ko"
+                ? "font-LINESeedKR"
+                : lang === "en"
+                ? "font-LINESeedJP"
+                : "font-LINESeedEN"
+            } ${
+                    event.source === "yoyogi"
+                      ? "bg-[#A2D4FF] hover:bg-[#70b9ff]"
+                      : event.source === "bread"
+                      ? "bg-[#FFD6A5] hover:bg-[#ffbe70]"
+                      : "bg-gray-300"
+                  }`}
+                >
+                  {lang === "ko"
+                    ? event.title_ko
+                    : lang === "ja"
+                    ? event.title_ja
+                    : event.title_en}
+                </a>
+
+                {/* 말풍선 (툴팁) */}
+                <div className="absolute bottom-full left-1/2 -translate-x-1/2 mb-2 hidden group-hover:block bg-white border border-gray-300 shadow-lg p-4 rounded-md z-50 w-[30rem] text-md whitespace-pre-wrap">
+                  <div className="flex mb-1">
+                    <span className="w-14 font-bold shrink-0">
+                      {Info[lang][0]}:
+                    </span>
+                    <span>
+                      {lang === "ko"
+                        ? event.location_ko
+                        : lang === "ja"
+                        ? event.location_ja
+                        : event.location_en}
+                    </span>
+                  </div>
+                  <div className="flex mb-1">
+                    <span className="w-14 font-bold shrink-0">
+                      {Info[lang][1]}:
+                    </span>
+                    <span>
+                      {event.date} {event.time}
+                    </span>
+                  </div>
+                  <div className="flex">
+                    <span className="w-14 font-bold shrink-0">
+                      {Info[lang][2]}:
+                    </span>
+                    <span className="whitespace-pre-wrap">
+                      {lang === "ko"
+                        ? event.description_ko
+                        : lang === "ja"
+                        ? event.description_ja
+                        : event.description_en}
+                    </span>
+                  </div>
+                </div>
+              </div>
             ))
           ) : (
             <p className="text-gray-500 text-center">
