edd987d79a6e34bbfb5268b644bb604728d76514
Boyeon Jang <uko010199@gmail.com>
2025-03-31 23:29:40 +0900

    chore: remove commented-out code
    - Cleaned up unused commented-out code to improve readability

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-// import Image from "next/image";
 import Header from "@/components/header/header";
 import Footer from "@/components/footer/footer";
 import Calendar from "@/components/calendar/calendar";
